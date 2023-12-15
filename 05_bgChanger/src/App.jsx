5135c3ee262006eb9522be94906a664fd272d32a
Muhammad Imran <imran5244513@gmail.com>
2023-12-15 08:55:28 -0800

    Adding new project bgChanger https://github.com/Imran5244/React_Learning_and_Projects.git

---
+++ 05_bgChanger/src/App.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const colors = [
+    'red',
+    'green',
+    'yellow',
+    'blue',
+    'orange',
+    'pink',
+    'black',
+    'white',
+    'purple',
+  ];
+  const [color, setColor] = useState('olive');
+
+  useEffect(() => {
+    // Function to change color after 3 seconds
+    const changeColorAfterDelay = () => {
+      const randomColor = colors[Math.floor(Math.random() * colors.length)];
+      setColor(randomColor);
+    };
+
+    // Set up the timer to change color every 3 seconds
+    const timer = setInterval(changeColorAfterDelay, 2000);
+
+    // Clean up the timer on component unmount
+    return () => clearInterval(timer);
+  }, []); // Empty dependency array means this effect runs once when the component mounts
+
+  return (
+    <div
+      className='w-full h-screen duration-200'
+      style={{ backgroundColor: color }}
+    >
+      <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+          <button
+            onClick={() => setColor('red')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'red' }}
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor('green')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'green' }}
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor('yellow')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'yellow' }}
+          >
+            Yeloow
+          </button>
+          <button
+            onClick={() => setColor('blue')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'blue' }}
+          >
+            Blue
+          </button>
+          <button
+            onClick={() => setColor('orange')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'orange' }}
+          >
+            Orange
+          </button>
+          <button
+            onClick={() => setColor('pink')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'pink' }}
+          >
+            Pink
+          </button>
+          <button
+            onClick={() => setColor('black')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'black' }}
+          >
+            Black
+          </button>
+          <button
+            onClick={() => setColor('white')}
+            className='outline-none px-4 py-1 rounded-full text-black shadow-lg '
+            style={{ backgroundColor: 'white' }}
+          >
+            White
+          </button>
+          <button
+            onClick={() => setColor('purple')}
+            className='outline-none px-4 py-1 rounded-full text-white shadow-lg '
+            style={{ backgroundColor: 'purple' }}
+          >
+            Purple
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;

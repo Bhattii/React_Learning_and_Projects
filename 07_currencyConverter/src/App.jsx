651236d42fbf6411569164dbac4b258e6b21c1f6
Muhammad Imran <imran5244513@gmail.com>
2023-12-18 10:00:48 -0800

    custom hooks and currency converter project

---
+++ 07_currencyConverter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>Currency Converter</h1>
+    </>
+  );
+}
+
+export default App;

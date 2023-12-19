9ff5c6a175087ba6588137ec87807c05abc1437c
Muhammad Imran <imran5244513@gmail.com>
2023-12-19 00:27:35 -0800

    Currency Converter Project Upload

--- 07_currencyConverter/src/App.jsx
+++ 07_currencyConverter/src/App.jsx
@@ -1,12 +1,93 @@
-import { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { InputBox } from './components';
+import useCurrencyInfo from './hooks/useCurrencyInfo';
 
 import './App.css';
 
 function App() {
+  const [amount, setAmount] = useState('');
+  const [from, setFrom] = useState('usd');
+  const [to, setTo] = useState('pkr');
+  const [convertedAmount, setConvertedAmount] = useState();
+  const currencyInfo = useCurrencyInfo(from);
+  const options = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setFrom(to);
+    setTo(from);
+  };
+
+  const convert = () => {
+    const amountValue = parseFloat(amount);
+    setConvertedAmount(amountValue * currencyInfo[to]);
+  };
+
+  const handleAmountChange = (newAmount) => {
+    if (newAmount >= 0 || newAmount === '') {
+      setAmount(newAmount);
+    }
+  };
+
+  useEffect(() => {
+    convert();
+  }, [amount, from, to, currencyInfo]);
+
   return (
-    <>
-      <h1 className='text-3xl bg-orange-500'>Currency Converter</h1>
-    </>
+    <div
+      className='w-full h-screen flex flex-wrap justify-center items-center bg-cover'
+      style={{
+        backgroundImage: 'url("")',
+      }}
+    >
+      <div className='w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat'>
+        <div className='w-full'>
+          <div className='w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30'>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                convert();
+              }}
+            >
+              <div className='w-full mb-1'>
+                <InputBox
+                  label='From'
+                  amount={amount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  selectCurrency={from}
+                  onAmountChange={handleAmountChange}
+                />
+              </div>
+              <div className='relative w-full h-0.5'>
+                <button
+                  type='button'
+                  className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
+                  onClick={swap}
+                >
+                  swap
+                </button>
+              </div>
+              <div className='w-full mt-1 mb-4'>
+                <InputBox
+                  label='To'
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                  amountDisable
+                />
+              </div>
+              <button
+                type='submit'
+                className='w-full bg-blue-600 text-white px-4 py-3 rounded-lg'
+              >
+                Convert {from.toUpperCase()} to {to.toUpperCase()}
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- 07_currencyConverter/src/hooks/useCurrencyInfo.js
+++ 07_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -9,7 +9,7 @@ function useCurrencyInfo(currency) {
       .then((res) => res.json())
       .then((res) => setData(res[currency]));
   }, [currency]);
-  console.log(data);
+
   return data;
 }
 export default useCurrencyInfo;

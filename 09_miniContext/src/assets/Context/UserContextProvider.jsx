33004c9694d11933c6abbeb6cd6cf6909d1963fb
Muhammad Imran <imran5244513@gmail.com>
2023-12-21 23:34:34 +0500

    Context API with mini Context Project

---
+++ 09_miniContext/src/assets/Context/UserContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import UserContext from './context';
+
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState(null);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextProvider;

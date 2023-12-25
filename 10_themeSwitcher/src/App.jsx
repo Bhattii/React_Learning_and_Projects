347adc6f48673fcc080c814ade7f879e6a91255e
Muhammad Imran <imran5244513@gmail.com>
2023-12-25 23:36:48 +0500

    themeSwitcher

---
+++ 10_themeSwitcher/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className='flex flex-wrap min-h-screen items-center'>
+        <div className='w-full'>
+          <div className='w-full max-w-sm mx-auto flex justify-end mb-4'>
+            {/* Theme Button */}
+          </div>
+
+          <div className='w-full max-w-sm mx-auto'>{/* Card */}</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ 10_themeSwitcher/src/Context/Theme.jsx
@@ -0,0 +1,5 @@
+import React, { createContext, useContext } from 'react';
+
+const themeContext = React.createContext();
+
+export default themeContext;

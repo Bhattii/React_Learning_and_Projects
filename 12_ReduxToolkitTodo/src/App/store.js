767030bf33659d95ec3adc839e6e3feea4d4ac36
Muhammad Imran <imran5244513@gmail.com>
2024-01-16 10:41:08 +0500

    Redux Toolkit Todo

--- 12_ReduxToolkitTodo/src/App/store.js
+++ 12_ReduxToolkitTodo/src/App/store.js
@@ -1,2 +1,6 @@
-import { configureStore } from '@reduxjs/toolkit';
-export const store = configureStore({});
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../features/todo/todoSlice';
+
+export const store = configureStore({
+    reducer: todoReducer
+})

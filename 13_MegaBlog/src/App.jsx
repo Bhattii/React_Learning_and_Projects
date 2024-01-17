bc9fc0bc5eb4bc9bf6fd409f790e94769938d7ee
Muhammad Imran <imran5244513@gmail.com>
2024-01-17 20:03:27 +0500

    Mega Blog App

---
+++ 13_MegaBlog/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL);
+  return (
+    <>
+      <h1>A Blog App with AppWrite</h1>
+    </>
+  );
+}
+
+export default App;

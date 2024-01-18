ca3380272ffb0b45a1c6beddf932bc27f7b31e62
Muhammad Imran <imran5244513@gmail.com>
2024-01-18 16:28:45 +0500

    adding .env, env.sample, conf.js files. These files store private variables

--- 13_MegaBlog/src/App.jsx
+++ 13_MegaBlog/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 
 function App() {
-  console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>A Blog App with AppWrite</h1>

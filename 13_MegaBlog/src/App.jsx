2183bc55ec62f4312d247aef909b257b0bfc049b
Muhammad Imran <imran5244513@gmail.com>
2024-01-21 13:59:58 +0500

    App Store

--- 13_MegaBlog/src/App.jsx
+++ 13_MegaBlog/src/App.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import { login, logout } from './appwrite/auth';
+import { Header, Footer } from './components/index';
 import './App.css';
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>A Blog App with AppWrite</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+  return !loading ? (
+    <div className='min-h-screen flex-wrape content-between bg-color-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>{/* <Outlet /> */}</main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;

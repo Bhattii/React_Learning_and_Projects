3a3c5ef2118ea008e17d7a511535f55c7388cc19
Muhammad Imran <imran5244513@gmail.com>
2024-04-05 16:05:24 +0500

    update

---
+++ 14_ReactComplete/src/components/Componets.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Componets() {
+  return <div>Componets</div>;
+}
+
+export default Componets;
---
+++ 14_ReactComplete/src/components/Props.jsx
@@ -0,0 +1,5 @@
+function Props() {
+  return <div>props</div>;
+}
+
+export default Props;

ee2ad96531dba63bba265ba8d5e007fd6a975146
Muhammad Imran <imran5244513@gmail.com>
2024-04-16 15:05:34 +0500

    update

--- 14_ReactComplete/src/components/Props.jsx
+++ 14_ReactComplete/src/components/Props.jsx
@@ -1,9 +1,20 @@
-export const Props = (p) => {
+// export const Props = (p) => {
+//   return (
+//     <>
+//       <h1>Learn Props</h1>
+//       <h1>{p.name}</h1>
+//       <h2>{p.roll}</h2>
+//     </>
+//   );
+// };
+
+// Object Destructing
+export const Props = ({ name, roll }) => {
   return (
     <>
       <h1>Learn Props</h1>
-      <h1>{p.name}</h1>
-      <h2>{p.roll}</h2>
+      <h1>{name}</h1>
+      <h2>{roll}</h2>
     </>
   );
 };

00469d48c0b9212000d62026f40d1bebbf5dc296
Bhattii <imran5244513@gmail.com>
2024-10-21 07:54:19 -0700

    List

--- React_Learning/06_List/src/App.jsx
+++ React_Learning/06_List/src/App.jsx
@@ -1,9 +1,30 @@
 import List from './List';
 
 function App() {
+	const fruits = [
+		{ id: 1, name: 'Apple', calories: 90 },
+		{ id: 2, name: 'banana', calories: 45 },
+		{ id: 3, name: 'mango', calories: 105 },
+		{ id: 4, name: 'coconut', calories: 159 },
+		{ id: 5, name: 'pineapple', calories: 37 },
+	];
+	const vegatables = [
+		{ id: 7, name: 'Potatos', calories: 105 },
+		{ id: 8, name: 'celery', calories: 15 },
+		{ id: 9, name: 'carrots', calories: 25 },
+		{ id: 10, name: 'corn', calories: 63 },
+		{ id: 11, name: 'broccoli', calories: 50 },
+	];
 	return (
 		<>
-			<List />
+			<List
+				items={fruits}
+				category="fruits"
+			/>
+			<List
+				items={vegatables}
+				category="Vegatables"
+			/>
 		</>
 	);
 }
--- React_Learning/06_List/src/List.jsx
+++ React_Learning/06_List/src/List.jsx
@@ -1,5 +1,20 @@
-function List() {
-	const fruits = ['Apple', 'banana', 'coconut', 'pineapple'];
-	return fruits;
+function List(props) {
+	const category = props.category;
+	const itemList = props.items;
+
+	const listItems = itemList.map((item) => (
+		<li key={item.id}>
+			{item.id} ) &nbsp;
+			{item.name}: &nbsp; {item.calories}
+		</li>
+	));
+	return (
+		<>
+			<h1>
+				<b>{category}</b>
+			</h1>
+			<ol>{listItems}</ol>
+		</>
+	);
 }
 export default List;
